4415923d94377783aad2984e951981b85ce12b99
RimmiKaur <rimmikaur37@gmail.com>
2025-04-26 23:56:46 +0530

    fifth commit

--- backend/utils/upload.js
+++ backend/utils/upload.js
@@ -42,6 +42,10 @@ const fileExistsInDirectory = async (filePath) => {
 
 // Controller to handle file upload
 const uploadFile = async (req, res) => {
+    if(req.file){
+        return res.status(400).send("No files uploadasfsdfsdfsddfs");
+
+    }
     if (!req.files || req.files.length === 0) {
         return res.status(400).send("No files uploaded");
     }
