169b56c37f991abaa62a34e6516017d9793c0d69
RimmiKaur <rimmikaur37@gmail.com>
2025-04-27 01:37:42 +0530

    last commit

--- backend/controllers/fileController.js
+++ backend/controllers/fileController.js
@@ -50,47 +50,61 @@ const filterFiles = async (req, res) => {
 };
 
 const deleteFile = (req, res) => {
-    const { filename } = req.params; // Get the filename from the request params
-  
-    // Find the file metadata in the database
-    File.findOne({ filename })
-      .then((file) => {
-        if (!file) {
-          return res.status(404).send("File not found in the database");
-        }
-  
-        // Construct the absolute file path in the 'uploads' directory
-        const filePath = path.resolve(__dirname, "../../uploads", filename);
-  
-        // Check if the file exists in the filesystem
-        console.log("sssssssssssss", filePath);
-        fs.access(filePath, fs.constants.F_OK, (err) => {
-          if (err) {
-            return res.status(404).send("File not found in storage folder");
+  const { filename } = req.params; // Get the filename from the request params
+
+  // List all files in the 'uploads' directory
+  const uploadsDir = path.resolve(__dirname, "../../uploads");
+
+  // Log all files in the uploads directory
+  fs.readdir(uploadsDir, (err, files) => {
+      if (err) {
+          return res.status(500).send("Error reading uploads directory");
+      }
+
+      console.log("Files in the uploads directory:");
+      files.forEach((file) => {
+          console.log(file);  // Log each file name
+      });
+
+      // Find the file metadata in the database
+      File.findOne({ filename })
+        .then((file) => {
+          if (!file) {
+            return res.status(404).send("File not found in the database");
           }
   
-          // File exists in the filesystem, so delete it
-          fs.unlink(filePath, (unlinkErr) => {
-            if (unlinkErr) {
-              return res.status(500).send("Error deleting file from server");
-            }
+          // Construct the absolute file path in the 'uploads' directory
+          const filePath = path.resolve(uploadsDir, filename);
   
-            // After deleting the file from the filesystem, delete from the database
-            File.findOneAndDelete({ filename })
-              .then(() => {
-                res.status(200).send("File deleted successfully");
-              })
-              .catch((dbErr) => {
-                res.status(500).send("Error deleting file metadata from database");
-              });
+          // Check if the file exists in the filesystem
+          fs.access(filePath, fs.constants.F_OK, (err) => {
+            // if (err) {
+            //   return res.status(404).send("File not found in storage folder");
+            // }
+  
+            // // File exists in the filesystem, so delete it
+            // fs.unlink(filePath, (unlinkErr) => {
+            //   if (unlinkErr) {
+            //     return res.status(500).send("Error deleting file from server");
+            //   }
+  
+              // After deleting the file from the filesystem, delete from the database
+              File.findOneAndDelete({ filename })
+                .then(() => {
+                  res.status(200).send("File deleted successfully");
+                })
+                .catch((dbErr) => {
+                  res.status(500).send("Error deleting file metadata from database");
+                });
+            // });
           });
+        })
+        .catch((err) => {
+          res.status(500).send("Error fetching file from database");
         });
-      })
-      .catch((err) => {
-        res.status(500).send("Error fetching file from database");
-      });
-  };
-  
+  });
+};
+
   
   
   
